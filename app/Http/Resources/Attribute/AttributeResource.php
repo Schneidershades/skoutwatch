8a84ea6efb774362e4c0c8c3ca4893654302dcbd
schneidershades <schneidershades@gmail.com>
2023-08-22 00:06:41 +0100

    or where transaction to transactions

--- app/Http/Resources/Attribute/AttributeResource.php
+++ app/Http/Resources/Attribute/AttributeResource.php
@@ -14,6 +14,9 @@ class AttributeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'score' => 70
+        ];
     }
 }
